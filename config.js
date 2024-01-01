6d6f1a5697a95d4c83bd842c8a071e1e4206848b
Jharnajyotidas <154789663+Jharnajyotidas@users.noreply.github.com>
2024-01-01 17:56:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
+global.owner = [ Hinata
+  ['918473891245', 'Hinata's bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
